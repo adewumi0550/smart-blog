c7fada85f8c004945698d9792e3f52c067c17e94
Adewumi Saheed Adewale <adewumiadewale493@gmail.com>
2020-11-02 03:19:34 +0100

    React Basic

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Header extends Component{
+  state = {
+
+  }
+  
+  render(){
+    return(
+      <div>
+         Header
+      </div>
+    )
+  }
+}
+
+export default Header
